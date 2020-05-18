870cf7e35fa44ed78fdf9f85123213b119367fa7
Anri Asaturov <anri82@gmail.com>
2020-05-19 02:25:20 +0300

    Bar.ts

---
+++ src/Bar.test.ts
@@ -0,0 +1,13 @@
+﻿import { Bar } from './Bar';
+
+test('Bar::constructor', () => {
+  const inst = new Bar('abc', 1, 2, 3, 4, 5, 6, 7);
+  expect(inst.Time).toBe('abc');
+  expect(inst.Open).toBe(1);
+  expect(inst.High).toBe(2);
+  expect(inst.Low).toBe(3);
+  expect(inst.Close).toBe(4);
+  expect(inst.Volume).toBe(5);
+  expect(inst.Count).toBe(6);
+  expect(inst.WAP).toBe(7);
+});
---
+++ src/Bar.ts
@@ -0,0 +1,16 @@
+﻿/**
+ * @brief The historical data bar's description.
+ * @sa EClient, EWrapper
+ */
+export class Bar {
+  constructor(
+    public readonly Time: string,
+    public readonly Open: number,
+    public readonly High: number,
+    public readonly Low: number,
+    public readonly Close: number,
+    public readonly Volume: number,
+    public readonly Count: number,
+    public readonly WAP: number
+  ) {}
+}
