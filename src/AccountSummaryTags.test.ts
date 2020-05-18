5b197927eff186283f554a0c57ddadf24cddf38f
Anri Asaturov <anri82@gmail.com>
2020-05-19 02:12:36 +0300

    AccountSummaryTags.ts

---
+++ src/AccountSummaryTags.test.ts
@@ -0,0 +1,19 @@
+﻿import { AccountSummaryTags } from './AccountSummaryTags';
+
+test('AccountSummaryTags::GetAllTags', () => {
+  const inst = new AccountSummaryTags();
+
+  let expected = '';
+  for (let prop in inst) {
+    if (prop === 'GetAllTags') continue;
+    if (expected) expected += ',';
+    //@ts-ignore
+    expected += inst[prop];
+  }
+
+  const actual = inst.GetAllTags();
+
+  expect(typeof actual).toBe('string');
+  expect(actual.length).toBeGreaterThan(0);
+  expect(expected.length).toBe(actual.length);
+});
---
+++ src/AccountSummaryTags.ts
@@ -0,0 +1,96 @@
+﻿/**
+ * @brief class containing all existing values being reported by EClientSocket::reqAccountSummary
+ */
+export class AccountSummaryTags {
+  AccountType = 'AccountType';
+  NetLiquidation = 'NetLiquidation';
+  TotalCashValue = 'TotalCashValue';
+  SettledCash = 'SettledCash';
+  AccruedCash = 'AccruedCash';
+  BuyingPower = 'BuyingPower';
+  EquityWithLoanValue = 'EquityWithLoanValue';
+  PreviousEquityWithLoanValue = 'PreviousEquityWithLoanValue';
+  GrossPositionValue = 'GrossPositionValue';
+  ReqTEquity = 'ReqTEquity';
+  ReqTMargin = 'ReqTMargin';
+  SMA = 'SMA';
+  InitMarginReq = 'InitMarginReq';
+  MaintMarginReq = 'MaintMarginReq';
+  AvailableFunds = 'AvailableFunds';
+  ExcessLiquidity = 'ExcessLiquidity';
+  Cushion = 'Cushion';
+  FullInitMarginReq = 'FullInitMarginReq';
+  FullMaintMarginReq = 'FullMaintMarginReq';
+  FullAvailableFunds = 'FullAvailableFunds';
+  FullExcessLiquidity = 'FullExcessLiquidity';
+  LookAheadNextChange = 'LookAheadNextChange';
+  LookAheadInitMarginReq = 'LookAheadInitMarginReq';
+  LookAheadMaintMarginReq = 'LookAheadMaintMarginReq';
+  LookAheadAvailableFunds = 'LookAheadAvailableFunds';
+  LookAheadExcessLiquidity = 'LookAheadExcessLiquidity';
+  HighestSeverity = 'HighestSeverity';
+  DayTradesRemaining = 'DayTradesRemaining';
+  Leverage = 'Leverage';
+
+  GetAllTags() {
+    return (
+      this.AccountType +
+      ',' +
+      this.NetLiquidation +
+      ',' +
+      this.TotalCashValue +
+      ',' +
+      this.SettledCash +
+      ',' +
+      this.AccruedCash +
+      ',' +
+      this.BuyingPower +
+      ',' +
+      this.EquityWithLoanValue +
+      ',' +
+      this.PreviousEquityWithLoanValue +
+      ',' +
+      this.GrossPositionValue +
+      ',' +
+      this.ReqTEquity +
+      ',' +
+      this.ReqTMargin +
+      ',' +
+      this.SMA +
+      ',' +
+      this.InitMarginReq +
+      ',' +
+      this.MaintMarginReq +
+      ',' +
+      this.AvailableFunds +
+      ',' +
+      this.ExcessLiquidity +
+      ',' +
+      this.Cushion +
+      ',' +
+      this.FullInitMarginReq +
+      ',' +
+      this.FullMaintMarginReq +
+      ',' +
+      this.FullAvailableFunds +
+      ',' +
+      this.FullExcessLiquidity +
+      ',' +
+      this.LookAheadNextChange +
+      ',' +
+      this.LookAheadInitMarginReq +
+      ',' +
+      this.LookAheadMaintMarginReq +
+      ',' +
+      this.LookAheadAvailableFunds +
+      ',' +
+      this.LookAheadExcessLiquidity +
+      ',' +
+      this.HighestSeverity +
+      ',' +
+      this.DayTradesRemaining +
+      ',' +
+      this.Leverage
+    );
+  }
+}
