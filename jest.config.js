dc0107c0672907430adb4ce6e138244fbc86843c
Anri Asaturov <anri82@gmail.com>
2020-05-19 20:29:12 +0300

    Add coverage output

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageReporters: [
+    //"json",
+    //'text',
+    'lcov'
+    //'clover'
+  ],
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/']
 };
