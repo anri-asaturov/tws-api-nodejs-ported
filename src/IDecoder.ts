61b2bfa8e749e8e5dc76d3c8986a032242df529d
Anri Asaturov <anri82@gmail.com>
2020-05-19 21:57:37 +0300

    IDecoder

---
+++ src/IDecoder.ts
@@ -0,0 +1,9 @@
+﻿export interface IDecoder {
+  ReadDouble(): number;
+  ReadDoubleMax(): number;
+  ReadLong(): number;
+  ReadInt(): number;
+  ReadIntMax(): number;
+  ReadBoolFromInt(): number;
+  ReadString(): number;
+}
