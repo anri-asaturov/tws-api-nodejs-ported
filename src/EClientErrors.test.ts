ee8c4854bb55e084a00bb7a72eaeff3ab260bd68
Anri Asaturov <anri82@gmail.com>
2020-05-19 21:17:11 +0300

    increase EClientError test coverage

--- src/EClientErrors.test.ts
+++ src/EClientErrors.test.ts
@@ -1,6 +1,10 @@
-import { EClientErrors } from './EClientErrors';
+import { EClientErrors, CodeMsgPair } from './EClientErrors';
 
 test('EClientErrors', () => {
   expect(EClientErrors.AlreadyConnected.Code).toBe(501);
   expect(EClientErrors.AlreadyConnected.Message).toBe('Already Connected.');
+
+  const pair = new CodeMsgPair();
+  expect(pair.Code).toBe(0);
+  expect(pair.Message).toBe('');
 });
