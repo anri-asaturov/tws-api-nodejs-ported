94fb97485ccdab7fa3ce8a66cfa3ab3fc28997a5
Anri Asaturov <anri82@gmail.com>
2020-05-19 03:16:16 +0300

    ComboLeg.ts

---
+++ src/ComboLeg.test.ts
@@ -0,0 +1,34 @@
+﻿import { ComboLeg } from './ComboLeg';
+
+test('ComboLeg::constructor empty', () => {
+  const leg = new ComboLeg();
+  expect(leg.ConId).toBe(0);
+  expect(leg.Ratio).toBe(0);
+  expect(leg.Action).toBe('');
+  expect(leg.Exchange).toBe('');
+  expect(leg.OpenClose).toBe(0);
+  expect(leg.ShortSaleSlot).toBe(0);
+  expect(leg.DesignatedLocation).toBe(0);
+  expect(leg.ExemptCode).toBe(0);
+  expect(Object.keys(leg).length).toBe(8);
+});
+
+test('ComboLeg::constructor non-empty', () => {
+  const leg = new ComboLeg(1, 2, 'a', 'b', 3, 4, 5, 6);
+  expect(leg.ConId).toBe(1);
+  expect(leg.Ratio).toBe(2);
+  expect(leg.Action).toBe('a');
+  expect(leg.Exchange).toBe('b');
+  expect(leg.OpenClose).toBe(3);
+  expect(leg.ShortSaleSlot).toBe(4);
+  expect(leg.DesignatedLocation).toBe(5);
+  expect(leg.ExemptCode).toBe(6);
+  expect(Object.keys(leg).length).toBe(8);
+});
+
+test('ComboLeg static props', () => {
+  expect(ComboLeg.SAME).toBe(0);
+  expect(ComboLeg.OPEN).toBe(1);
+  expect(ComboLeg.CLOSE).toBe(2);
+  expect(ComboLeg.UNKNOWN).toBe(3);
+});
---
+++ src/ComboLeg.ts
@@ -0,0 +1,21 @@
+﻿/**
+ * @brief Class representing a leg within combo orders.
+ * @sa Order
+ */
+export class ComboLeg {
+  public static SAME = 0;
+  public static OPEN = 1;
+  public static CLOSE = 2;
+  public static UNKNOWN = 3;
+
+  constructor(
+    public ConId = 0,
+    public Ratio = 0,
+    public Action = '',
+    public Exchange = '',
+    public OpenClose = 0,
+    public ShortSaleSlot = 0,
+    public DesignatedLocation = 0,
+    public ExemptCode = 0
+  ) {}
+}
